3b763374ee0e254373244085f086e0a0dc0e409c
Ruben Baskaran <rbaskaran92@gmail.com>
2020-02-17 15:15:37 +0100

    added play again button

--- colorGame.js
+++ colorGame.js
@@ -5,6 +5,21 @@ var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var message = document.querySelector("#message");
 var h1 = document.querySelector("h1");
+var resetButton = document.querySelector("#reset");
+
+resetButton.addEventListener("click", function ()
+{
+    colors = generateRandomColors(6);
+    pickedColor = pickColor();
+    colorDisplay.textContent = pickedColor;
+
+    for (var i = 0; i < squares.length; i++)
+    {
+        squares[i].style.backgroundColor = colors[i];
+    }
+
+    h1.style.backgroundColor = "#232323";    
+});
 
 colorDisplay.textContent = pickedColor;
 
@@ -19,6 +34,7 @@ for (var i = 0; i < squares.length; i++)
         if (clickedColor === pickedColor)
         {
             message.textContent = "Correct!";
+            resetButton.textContent = "Play again?";
             changeColors(clickedColor);
             h1.style.backgroundColor = clickedColor;
         }
